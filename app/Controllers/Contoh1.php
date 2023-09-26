37a25d646a1a9fcd805d654911e34fa6c453bcde
graywolfking07 <mjayaw07@gmail.com>
2023-09-26 19:47:28 +0700

    menambahkan Tugas

---
+++ app/Controllers/Contoh1.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+
+class Contoh1 extends BaseController
+{
+    public function index()
+    {
+        echo "<h1>Kontol</h1>";
+    }
+}
