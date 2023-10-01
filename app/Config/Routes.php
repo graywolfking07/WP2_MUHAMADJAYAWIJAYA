5c38464f5a9cf7094cb860a0941cca23bcc7cf50
graywolfking07 <mjayaw07@gmail.com>
2023-10-01 16:32:48 +0700

    Tugas Parsing Data

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,5 +5,7 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Latihan1::index');
+// $routes->get('/', 'Latihan1::index');
 $routes->get('/penjumlahan/(:num)/(:num)', 'Latihan1::getPenjumlahan/$1/$2');
+$routes->get('/', 'Matakuliah::index');
+$routes->post('/', 'Matakuliah::cetak');
---
+++ app/Controllers/Matakuliah.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+
+class Matakuliah extends BaseController
+{
+    public function index()
+    {
+        return view('view-form-matakuliah');
+    }
+
+    public function cetak()
+    {
+        // dd($this->request->getPost());
+        $data = [
+            'kode' => $this->request->getPost('kode'),
+            'nama' => $this->request->getPost('nama'),
+            'sks' => $this->request->getPost('sks')
+        ];
+
+        return view('view-data-matakuliah',$data);
+    }
+}
