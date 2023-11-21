a5162d1e0a7eccdd8ce8a0caaf684deb4a382385
graywolfking07 <mjayaw07@gmail.com>
2023-11-21 10:24:24 +0700

    tugas pertemuan 9

--- app/Models/ModelUser.php
+++ app/Models/ModelUser.php
@@ -28,4 +28,8 @@ class ModelUser extends CI_Model
         $this->db->limit(10, 0);
         return $this->db->get();
     }
+    public function cekData($where = null)
+ {
+    return $this->db->get_where('user', $where);
+}
 }
